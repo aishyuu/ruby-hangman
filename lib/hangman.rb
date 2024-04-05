f5ea497c8c132aba153ad58294cd1623b2893c1d
aishyuu <ivanmatias1331@gmail.com>
2024-04-04 17:40:26 -0700

    Loading file and data implemented

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,20 +1,43 @@
 require 'psych'
+require 'yaml'
+
+$fits = []
+File.foreach("hangman_words.txt") do |line|
+  fixed = line.chomp
+  if fixed.length.between?(5, 12)
+    $fits.push(fixed)
+  end
+end
 
 def start
   puts File.read("./lib/ascii_text.txt")
   puts "Type 'start' to begin New Game"
   puts "Type 'continue' to Load Game"
+  puts "Type 'exit' to Exit Game"
 
   usr_response = ""
-  until usr_response == "start" || usr_response == "continue" do
+  until usr_response == "start" || usr_response == "continue" || usr_response == "exit" do
     usr_response = gets.downcase.strip
   end
 
   if usr_response == "start"
-    puts "Starting game..."
+    fresh = new_game_setup
+    play_game(fresh)
   elsif usr_response == "continue"
     load_file
+  elsif usr_response == "exit"
+    exit
   end
+end
+
+def get_file_info(file_name)
+  return YAML.load(File.read("./saves/#{file_name}"))
+end
+
+def new_game_setup
+  prng = Random.new()
+  index = prng.rand($fits.length)
+  play_data = {info: {guesses:"", lives_left:6, word_progress:"_"*$fits[index].length, word_index:index}}
 end
 
 def load_file
@@ -34,7 +57,13 @@ def load_file
     usr_response = gets.downcase.strip
   end
 
-  puts "File chosen is #{usr_response}"
+  play_data = get_file_info("#{usr_response}.yaml")
+  play_game(play_data)
+end
+
+def play_game(data = {})
+  puts data
+  puts data["info"]
 end
 
 start
